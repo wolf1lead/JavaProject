1c8ca1a34db6ab025c0892331263e13257557a69
wolf1lead <wolf1lead@yahoo.com>
2018-12-14 20:42:46 -0600

    initial checkin
    just for test purposes

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main(string[] args)
+	{
+		system.out.println("Hello World!");
+		System.exit(0);
+		} // End of method main
+} // End of class HelloWorld
